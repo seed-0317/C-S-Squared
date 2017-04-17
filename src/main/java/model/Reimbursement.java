0f185c8855e49088354e3b7c51abd76b151e64b5
Mustanski <nof191@f45c89aa4f4d.cof.ds.capitalone.com>
2017-04-17 09:11:40 -0400

    add reimbursement class

---
+++ src/main/java/model/Reimbursement.java
@@ -0,0 +1,106 @@
+package model;
+
+import com.sun.tools.corba.se.idl.constExpr.Times;
+
+import java.sql.Blob;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+/**
+ * Created by nof191 on 4/17/17.
+ */
+public class Reimbursement {
+
+    private int id;
+    private float amount;
+    private String description;
+    private Blob receipt;
+    private Timestamp submitted;
+    private Timestamp resolved;
+    private int author;
+    private int resolver;
+    private int type;
+    private int status;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Blob getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(Blob receipt) {
+        this.receipt = receipt;
+    }
+
+    public Timestamp getSubmitted() {
+        return submitted;
+    }
+
+    public void setSubmitted(Timestamp submitted) {
+        this.submitted = submitted;
+    }
+
+    public Timestamp getResolved() {
+        return resolved;
+    }
+
+    public void setResolved(Timestamp resolved) {
+        this.resolved = resolved;
+    }
+
+    public int getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(int author) {
+        this.author = author;
+    }
+
+    public int getResolver() {
+        return resolver;
+    }
+
+    public void setResolver(int resolver) {
+        this.resolver = resolver;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
