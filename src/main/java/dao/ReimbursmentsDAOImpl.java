60f345e5af17933de15e57371856ed116d90c29c
Mustanski <nof191@f45c89aa4f4d.cof.ds.capitalone.com>
2017-04-19 16:32:58 -0400

    reimbursment impl minus save

--- src/main/java/dao/ReimbursmentsDAOImpl.java
+++ src/main/java/dao/ReimbursmentsDAOImpl.java
@@ -2,10 +2,7 @@ package dao;
 
 import model.Reimbursement;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,6 +70,47 @@ public class ReimbursmentsDAOImpl implements ReimbursmentsDAO {
 
     @Override
     public void createReimbursment(Reimbursement newReimbursment) throws Exception {
+        Connection connection =null;
+        PreparedStatement stmt = null;
+        int success = 0;
+        try {
+            connection = DAOUtilities.getConnection();
+            String sql = "INSERT INTO ERS_REIMBURSEMENTS VALUES (?,?,?,?,?,?,?,?,?,?)";
+
+            // Setup PreparedStatement
+            stmt = connection.prepareStatement(sql);
+
+            // Add parameters from reimbursment into PreparedStatement
+            stmt.setInt(1, newReimbursment.getId();
+            stmt.setString(2, newReimbursment.getAmount();
+            stmt.setString(3, newReimbursment.getDescription();
+            stmt.setBlob(4, newReimbursment.getReceipt();
+            stmt.setString(5, newReimbursment.getSubmitted();
+            stmt.setString(6, newReimbursment.getResolved();
+            stmt.setInt(7, newReimbursment.getAuthor());
+            stmt.setInt(8, newReimbursment.getResolver());
+            stmt.setInt(9, newReimbursment.getStatusID());
+            stmt.setInt(10, newReimbursment.getTypeId());
+
+            success = stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (success == 0) {
+            // then update didn't occur, throw an exception
+            throw new Exception("Insert feeding schedule failed: " + newReimbursment);
+        }
 
     }
 
