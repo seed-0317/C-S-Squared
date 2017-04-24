efbbf083af0cb623efd647121fd07076a56810ec
Sudhakar Chennupati <sudhakar.chennupati@gmail.com>
2017-04-24 18:46:51 -0400

    Updated Servlet logic to syncranise with html

--- src/main/java/servlets/ViewReimbursementServlet.java
+++ src/main/java/servlets/ViewReimbursementServlet.java
@@ -18,12 +18,12 @@ import java.util.List;
  */
 @WebServlet(urlPatterns = "/ListRis")
 public class ViewReimbursementServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        request.getRequestDispatcher("/views/login.html").forward(request, response);
-
-
-    }
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//        request.getRequestDispatcher("/views/login.html").forward(request, response);
+//
+//
+//    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -41,8 +41,9 @@ public class ViewReimbursementServlet extends HttpServlet {
         if (reimbursements != null) {
             System.out.println("Inside Session");
                 HttpSession session = request.getSession();
-                session.setAttribute("user", username);
-                response.sendRedirect("/views/allreimbursments.html");
+                session.setAttribute("reimbersment", reimbursements);
+            System.out.println("print ris"+ reimbursements);
+                request.getRequestDispatcher("/views/allreimbursments.html").forward(request,response);
 
             } else {
                 response.sendRedirect("login");
