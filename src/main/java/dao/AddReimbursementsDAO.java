33374a92951f968fffef9dc73f92e87bda5b93b4
Chad Blevins <chad.blevins@capitalone.com>
2017-04-25 15:55:24 -0400

    remove resolver

--- src/main/java/dao/AddReimbursementsDAO.java
+++ src/main/java/dao/AddReimbursementsDAO.java
@@ -8,20 +8,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class Test {
+public class AddReimbursementsDAO {
 
     public static void addReimburse(float amount, int author, int statusID, int TypeId, String descr,
-                                    String formattedDate, int idResolver) {
+                                    String formattedDate) {
         Connection connection = DAOUtilities.createConnection();
         try (PreparedStatement stmt1 = connection.prepareStatement("Insert INTO csssquared.ers_reimbursements " +
-                "(r_amount,u_id_author,rs_id, rt_id, r_description, r_submitted, u_id_resolver) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
+                "(r_amount,u_id_author,rs_id, rt_id, r_description) VALUES (?, ?, ?, ?, ?, ?)")) {
             stmt1.setFloat(1, amount);
             stmt1.setInt(2, author);
             stmt1.setInt(3, statusID);
             stmt1.setInt(4, TypeId);
             stmt1.setString(5, descr);
             stmt1.setString(6, formattedDate);
-            stmt1.setInt(7, idResolver);
             ResultSet rs = stmt1.executeQuery();
         } catch (SQLException e1) {
             e1.printStackTrace();
